f4c898c90ced608e11568fa1b5d4c5485d345c32
Anderson Nascimento <anderson.nascimento@meta.com.br>
2024-09-13 14:52:38 -0300

    feat: add page home

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,28 @@
+import { Plus } from 'lucide-react'
+
+import logo from './assets/logo-in-orbit.svg'
+import letsStart from './assets/lets-start-illustration.svg'
+
 export function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </>
+    <div className="h-screen flex flex-col items-center justify-center gap-8 ">
+      <img src={logo} alt="in.orbit" />
+      <img src={letsStart} alt="lets start illustration" />
+
+      <p
+        className="text-zinc-300 leading-relaxed max-w-80 text-center"
+      >
+        Você ainda não cadastrou nenhuma meta, que tal cadastrar uma agora mesmo?
+      </p>
+
+      <button
+        type="button"
+        className='flex gap-4 items-center bg-violet-500 px-4 py-2.5 rounded-lg text-violet-50 text-sm font-medium tracking-tight houver:bg-violet-600'
+      >
+        <Plus className='size-4 text-violet-50' />
+        Cadastrar meta
+      </button>
+    </div>
   )
 }
 
